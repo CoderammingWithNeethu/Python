f9eb8996ec6ff42fd7fb5ec3d2dd6bd61ed9983f
CoderammingWithNeethu <neethu1027@gmail.com>
2022-05-31 21:38:08 +0530

    optimized way

--- LeetCode/1_TwoSum.py
+++ LeetCode/1_TwoSum.py
@@ -11,14 +11,38 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 '''
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum_BruteForce(self, nums: list[int], target: int) -> list[int]:
         #Brute Force
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)): 
                 if (nums[i]+nums[j]) == target:
                     return [i,j]
 
+    #Optimizted Way
+    def twoSum_Hashmap(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        for i, value in enumerate(nums):
+           remaining = target - nums[i]
+           
+           if remaining in seen:
+               return [i, seen[remaining]]
+            
+           seen[value] = i 
+
+           '''
+           [7,2,13,11,8] ; target = 24
+           {}<--- hashmap
+           Required number = 24 - 7 = 17 is not present in our hashmap, so we add 7 as idxMap key and 0 as idxMap value 
+           (0 is  the index of 7 in input array) {7:0}; repeat
+           7- {7:0}
+           2- {7:0,2:1}
+           13- {7:0,2:1,13:2}
+           Now required number = 24 - 11 = 13 is present in idxMap
+           That means we have found the pair which adds up to the target sum 24, i.e. (11 , 13). 
+           11- {7:0,2:1,13:2} ---> 13 req number is present in hashmap 
+           '''
+
 obj = Solution()
-#obj.twoSum([2,7,11,15],9) #[1, 2]
-#obj.twoSum([3,3],6) #[0, 1]
+obj.twoSum([2,7,11,15],9) #[1, 2]
+obj.twoSum([3,3],6) #[0, 1]
 print(obj.twoSum([3,2,4],6))#[1, 2]
